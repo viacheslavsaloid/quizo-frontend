9d533fa6f9b8d41fa42b147ae493d9046c9385ba
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2020-11-14 10:06:56 +0200

    fix: token

--- src/app/settings/configs/jwt.configs.ts
+++ src/app/settings/configs/jwt.configs.ts
@@ -1,7 +1,9 @@
 import { environment } from 'src/environments/environment';
 
 export function tokenGetter() {
-  return JSON.parse(localStorage.getItem('configs') || `{ token: '' }`).token;
+  const configs = localStorage.getItem('configs');
+  const data = JSON.parse(configs);
+  return data?.token;
 }
 
 export const JWT_CONFIGS = {
